89acc0bf4fecd9e60407689088105acca3931dc4
Jonathan Nicholas <jonicrecis@gmail.com>
2018-08-04 23:48:19 +1000

    Roadmap feature parity

--- REST.py
+++ REST.py
@@ -9,7 +9,7 @@ app.config["MONGO_URI"] = "mongodb://meetmon-test:1testaccount@ds249311.mlab.com
 mongo = PyMongo(app)
 
 @app.route("/event",methods=["GET","POST"])
-def event_method():
+def events_method():
     if request.method == "GET":
         get_all()
     else:
@@ -25,8 +25,26 @@ def add_new_event():
     return jsonify(json.loads(dumps(result)))
 
 
-@app.route("/event/<ObjectId:id>")
+@app.route("/event/<ObjectId:id>",methods=["GET","PUT","DELETE"])
+def event_method():
+    if request.method=="GET":
+        get_details(id)
+    elif request.method=="PUT":
+        edit_details(id)
+    else:
+        delete_card(id)
+
 def get_details(id):
     results = mongo.db.event.find({"_id":id})
     return jsonify(json.loads(dumps(results)))
 
+def edit_details(id):
+    data_input = {'title':request.form['title'],'description':request.form['description']}
+    condition = {'_id':id}
+    result = mongo.db.event.update_one(condition,{'$inc':data_input})
+    return jsonify(json.loads(dumps(result)))
+
+def delete_card(id):
+    condition = {'_id':id}
+    result = mongo.db.event.delete_one(condition)
+    return jsonify(json.loads(dumps(result)))
