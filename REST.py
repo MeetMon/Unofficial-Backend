7b60d6b11862625d00e6bd1981a64feb8db7164e
Handy Hasan <hhas0002@student.monash.edu>
2018-08-10 11:48:58 +1000

    added photo to local

--- REST.py
+++ REST.py
@@ -5,12 +5,17 @@ from flask_cors import CORS, cross_origin
 from bson import ObjectId
 from bson.json_util import dumps
 from datetime import datetime
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config["MONGO_URI"] = "mongodb://meetmon-test:1testaccount@ds249311.mlab.com:49311/meetmon"
 mongo = PyMongo(app)
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static'
+configure_uploads(app, photos)
 
 def convert_result(mongo_result):
     mongo_result = list(mongo_result)
@@ -20,13 +25,14 @@ def convert_result(mongo_result):
         current_result['_id'] = str(item['_id'])
         current_result['title'] = item['title']
         current_result['description'] = item['description']
+        current_result['photo'] = item['photo']
         if 'timestamp' in item:
             current_result['timestamp'] = item['timestamp']
         result.append(current_result)
     return jsonify(result)
 
 #test
-@app.route("/event",methods=["GET","POST"])
+@app.route("/event/",methods=["GET","POST"])
 @cross_origin()
 def events_method():
     result = None
@@ -41,7 +47,8 @@ def get_all():
     return convert_result(results)
 
 def add_new_event():
-    data_input = {'title':request.form['title'],'description':request.form['description'],'timestamp':datetime.now()}
+    
+    data_input = {'title':request.form['title'],'description':request.form['description'],'timestamp':datetime.now(),'photo':photos.save(request.files['photo'])}
     result = mongo.db.event.insert_one(data_input)
     return jsonify({'_id':str(result.inserted_id)})
 
