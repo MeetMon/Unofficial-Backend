6e1395aa728190b5d49fd115346d6ec3dbc5a5db
Jonathan Nicholas <jonicrecis@gmail.com>
2018-08-23 13:18:36 +1000

    Image upload, and score basic schematics

--- REST.py
+++ REST.py
@@ -1,11 +1,12 @@
 import json
 import os
-from flask import Flask,jsonify,request
-from flask_pymongo import PyMongo
-from flask_cors import CORS, cross_origin
 from bson import ObjectId
 from bson.json_util import dumps
 from datetime import datetime
+from flask import Flask,jsonify,request,send_from_directory
+from flask_cors import CORS, cross_origin
+from flask_pymongo import PyMongo
+from PIL import Image
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -15,6 +16,8 @@ app.config["MONGO_URI"] = "mongodb://meetmon-test:1testaccount@ds249311.mlab.com
 mongo = PyMongo(app)
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+BASE_URL = 'http://localhost:5000/'
+IMAGE_SIZE = 500
 
 app.config['UPLOAD_FOLDER'] = 'static'
 
@@ -25,8 +28,9 @@ def convert_result(mongo_result):
         current_result = {}
         current_result['_id'] = str(item['_id'])
         current_result['title'] = item['title']
-        current_result['description'] = item['description']
-        current_result['filename'] = item['filename']
+        current_result['upvotes'] = item['upvotes']
+        current_result['downvotes'] = item['downvotes']
+        current_result['image'] = BASE_URL+'image/'+item['filename']
         current_result['timestamp'] = item['timestamp']
         result.append(current_result)
     return jsonify(result)
@@ -46,7 +50,7 @@ def get_all():
     return convert_result(results)
 
 def add_new_event():
-    data_input = {'title':request.form['title'],'description':request.form['description'],'timestamp':datetime.now(),'filename':request.form['filename']}
+    data_input = {'title':request.form['title'],'timestamp':datetime.now(),'filename':request.form['filename'],'upvotes':1,'downvotes':0}
     result = mongo.db.event.insert_one(data_input)
     return jsonify({'_id':str(result.inserted_id)})
 
@@ -67,12 +71,6 @@ def get_details(id):
     results = mongo.db.event.find({"_id":id})
     return convert_result(results)
 
-def edit_details(id):
-    data_input = {'title':request.form['title'],'description':request.form['description']}
-    condition = {'_id':id}
-    result = mongo.db.event.replace_one(condition,data_input)
-    return jsonify({'_id':str(id)})
-
 def delete_card(id):
     condition = {'_id':id}
     result = mongo.db.event.delete_one(condition)
@@ -88,5 +86,24 @@ def upload_file():
     file = request.files['image']
     if allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+        image = Image.open(filepath)
+        width, height = image.size
+        if (width > IMAGE_SIZE and height > IMAGE_SIZE):
+            image.resize((IMAGE_SIZE,IMAGE_SIZE)).save(filepath)
+        elif (width <= IMAGE_SIZE and height > IMAGE_SIZE):
+            image.resize((width,IMAGE_SIZE)).save(filepath)
+        elif (width > IMAGE_SIZE and height <= IMAGE_SIZE):
+            image.resize((IMAGE_SIZE,height)).save(filepath)
     return filename
+
+@app.route('/vote/<string:method>/<ObjectId:id>',methods=["POST"])
+@cross_origin()
+def vote(id):
+    pass
+
+@app.route('/image/<string:filename>')
+@cross_origin()
+def image_serve(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
